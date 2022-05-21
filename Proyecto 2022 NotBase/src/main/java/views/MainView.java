6a2811d66ab4a4c393d3a22f830c5cd93b0b5536
juan <juanmarten99@gmail.com>
2022-05-21 14:00:42 -0300

    Dummy view

--- Proyecto 2022 NotBase/src/main/java/views/MainView.java
+++ Proyecto 2022 NotBase/src/main/java/views/MainView.java
@@ -1,14 +1,32 @@
 package views;
 
+import dyds.gourmetCatalog.fulllogic.MainWindow;
 import presenter.GourmetCatalogPresenterInterface;
 
+import javax.swing.*;
 import java.util.List;
 
 public class MainView implements  MainViewInterface{
+
+    private JTextField textField1;
+    private JPanel contentPane;
+    private JTextPane textPane1;
+    private JButton saveLocallyButton;
+    private JTabbedPane tabbedPane1;
+    private JPanel searchPanel;
+    private JPanel storagePanel;
+    private JComboBox<Object> comboBox1;
+    private JTextPane textPane2;
+    private JFrame mainFrame;
     private GourmetCatalogPresenterInterface gourmetCatalogPresenter;
 
     public MainView(GourmetCatalogPresenterInterface gourmetCatalogPresenter){
         this.gourmetCatalogPresenter = gourmetCatalogPresenter;
+
+        mainFrame = new JFrame("Gourmet Catalog");
+        mainFrame.setContentPane(contentPane);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainFrame.pack();
     }
 
     @Override
@@ -57,7 +75,7 @@ public class MainView implements  MainViewInterface{
 
     @Override
     public void showView(){
-        System.out.println("Mostrar pantalla");
+        mainFrame.setVisible(true);
         //TODO
     }
 }
